30c4f0b84974dbc66efca93d3356d97fffa7fb7e
paulgo86 <ghseok86@gmail.com>
2020-09-18 13:58:35 +0900

    merge

--- test/index.js
+++ test/index.js
@@ -153,7 +153,7 @@ var test3 = async () => {
         let radioTab = await driver.findElement(By.id('button-image-mode'));
         radioTab.click();
         await delay(1);
-        let radioForChannel = 
+        
 
         let startTime = new Date().getTime();
         let duration = 120 // minute
